e744f4ef0198b3b2233ded307dd6fda64132d26f
Farkas Ferenc <85436006+FerencFarkas22@users.noreply.github.com>
2022-10-07 17:39:12 +0200

    Add files via upload

--- src/components/Projectek.js
+++ src/components/Projectek.js
@@ -15,8 +15,8 @@ const Projectek = () => {
                     <h1>DonCorleone Pizza Reactban</h1>
                     <p>Ez egy több oldalas, Routerrel megoldott React weboldal ami egy pizzázó weboldal felületét jeleníti meg</p>
                     <div className="buttons">
-                        <a href="https://github.com/FerencFarkas22/react-pizza/tree/30cbd4158a4662b1093304a9da8df7fcde5d0b55"><button>GitHub <GitHub /></button></a>
-                       <a href="https://ferencfarkas22.github.io/react-pizza/#/"><button>Weboldal megnyitása</button></a> 
+                        <a rel="noreferrer" target={'_blank'} href="https://github.com/FerencFarkas22/react-pizza/tree/30cbd4158a4662b1093304a9da8df7fcde5d0b55"><button>GitHub <GitHub /></button></a>
+                       <a rel="noreferrer" target={'_blank'} href="https://ferencfarkas22.github.io/react-pizza/#/"><button>Weboldal megnyitása</button></a> 
                     </div>
                 </div>
             </div>
@@ -25,8 +25,8 @@ const Projectek = () => {
                     <h1>Kocka Játék Javascriptel</h1>
                     <p>Kocka játék javascriptben! Ha egyest dobsz a pontod nullázódik</p>
                     <div className="buttons">
-                        <a target={'_blank'} href="https://github.com/FerencFarkas22/kockajatek"><button>GitHub <GitHub /></button></a>
-                        <a target={'_blank'} href="https://ferencfarkas22.github.io/kockajatek/"><button>Weboldal megnyitása</button></a>
+                        <a rel="noreferrer" target={'_blank'} href="https://github.com/FerencFarkas22/kockajatek"><button>GitHub <GitHub /></button></a>
+                        <a rel="noreferrer" target={'_blank'} href="https://ferencfarkas22.github.io/kockajatek/"><button>Weboldal megnyitása</button></a>
                     </div>
                 </div>
                 <div className="background-image"><img src={KockaJatek} alt="" /></div>
@@ -37,8 +37,8 @@ const Projectek = () => {
                     <h1>Harry Potter karakter kereső apiból</h1>
                     <p>Harry Potter api használatával kereshető ki karakter</p>
                     <div className="buttons">
-                        <a target={'_blank'} href="https://github.com/FerencFarkas22/Harry-Potter-searcher"><button>GitHub <GitHub /></button></a>
-                        <a target={'_blank'} href="https://ferencfarkas22.github.io/Harry-Potter-searcher/"><button>Weboldal megnyitása</button></a>
+                        <a rel="noreferrer" target={'_blank'} href="https://github.com/FerencFarkas22/Harry-Potter-searcher"><button>GitHub <GitHub /></button></a>
+                        <a rel="noreferrer" target={'_blank'} href="https://ferencfarkas22.github.io/Harry-Potter-searcher/"><button>Weboldal megnyitása</button></a>
                     </div>
                 </div>
             </div>
